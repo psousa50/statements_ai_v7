1a2868fd7940b8b5b66240219a2cf292720359dc
Pedro Sousa <pedronsousa@gmail.com>
2025-05-23 08:11:19 +0100

    refactor: consolidate schema detection logic using ConversionModel class

--- bank-statements-api/app/services/statement_processing/statement_analyzer.py
+++ bank-statements-api/app/services/statement_processing/statement_analyzer.py
@@ -2,6 +2,7 @@ import logging
 
 from app.domain.dto.statement_processing import AnalysisResultDTO
 from app.services.common import compute_hash
+from app.services.schema_detection.schema_detector import ConversionModel
 
 logger_content = logging.getLogger("app.llm.big")
 logger = logging.getLogger("app")
@@ -28,22 +29,17 @@ class StatementAnalyzerService:
         file_hash = compute_hash(filename, file_content)
         existing_metadata = self.file_analysis_metadata_repo.find_by_hash(file_hash)
 
+        file_type = self.file_type_detector.detect(file_content)
         if existing_metadata:
             raw_df = self.statement_parser.parse(file_content, existing_metadata.file_type)
-            file_type = existing_metadata.file_type
-            schema_info = self.schema_detector.detect_schema(raw_df)
-
-            column_mapping = schema_info.column_map
-            header_row_index = schema_info.header_row
-            data_start_row_index = schema_info.start_row
+            conversion_model = ConversionModel(
+                column_map=existing_metadata.column_mapping,
+                header_row=existing_metadata.header_row_index,
+                start_row=existing_metadata.data_start_row_index,
+            )
         else:
-            file_type = self.file_type_detector.detect(file_content)
             raw_df = self.statement_parser.parse(file_content, file_type)
-            schema_info = self.schema_detector.detect_schema(raw_df)
-
-            column_mapping = schema_info.column_map
-            header_row_index = schema_info.header_row
-            data_start_row_index = schema_info.start_row
+            conversion_model = self.schema_detector.detect_schema(raw_df)
 
         saved_file = self.uploaded_file_repo.save(filename, file_content, file_type)
         uploaded_file_id = saved_file.id
@@ -53,10 +49,17 @@ class StatementAnalyzerService:
         return AnalysisResultDTO(
             uploaded_file_id=uploaded_file_id,
             file_type=file_type,
-            column_mapping=column_mapping,
-            header_row_index=header_row_index,
-            data_start_row_index=data_start_row_index,
+            column_mapping=conversion_model.column_map,
+            header_row_index=conversion_model.header_row,
+            data_start_row_index=conversion_model.start_row,
             sample_data=sample_data,
+        )
+
+    def existing_metadata_to_schema_info(self, existing_metadata):
+        return SchemaInfo(
+            header_row=existing_metadata.header_row_index,
+            start_row=existing_metadata.data_start_row_index,
+            column_map=existing_metadata.column_mapping,
         )
 
     def _generate_sample_data(self, raw_df):
