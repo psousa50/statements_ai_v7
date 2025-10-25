7206a7654bcbc02ab432c83b12608bcbaf901cec
Pedro Sousa <pedronsousa@gmail.com>
2025-10-25 16:07:43 +0100

    fix eslint config

--- bank-statements-web/.eslintrc.cjs
+++ bank-statements-web/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    ecmaVersion: 2020,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'prettier'],
